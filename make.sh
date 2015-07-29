36521d1d842b55686ce1d4498d98f39ab202146a
Mariusz Gorski <marius.gorski@gmail.com>
2015-07-29 23:43:54 +0200

    Fix build script

--- make.sh
+++ make.sh
@@ -2,8 +2,8 @@
 
 # A shortcut script for cross-compilation for Raspberry Pi
 
-KERNELDIR=../linux
-ARCH=arm
-CROSS_COMPILE=../tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-
+export KERNELDIR=../linux
+export ARCH=arm
+export CROSS_COMPILE=../tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-
 make $@
 
